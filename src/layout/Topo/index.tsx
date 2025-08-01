4a7a2cd03224a4af76d83ca6c90a623e58b3ebca
kali <felipevasc@hotmail.com.br>
2025-07-31 23:45:40 -0300

    incluir consulta a projetos

--- src/layout/Topo/index.tsx
+++ src/layout/Topo/index.tsx
@@ -1,11 +1,15 @@
 "use client"
 import useApi from "@/api";
 import { StyledTopo } from "./styles";
+import { Button, Menu } from "@/common/components";
 
 const Topo = () => {
     const api  = useApi();
     const { data, error, isLoading } = api.projeto.getProjetos();
-    return <StyledTopo>Projetos: {JSON.stringify(data)}</StyledTopo>
+    return <StyledTopo>
+        <div>Topo</div>
+        <div>{data?.map(p => <Button>{p.nome}</Button>)}</div>
+    </StyledTopo>
 }
 
 export default Topo;
