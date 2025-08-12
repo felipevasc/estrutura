36d429fe0e4e72b71ba40db45a3a43acb1f0f5a4
kali <felipevasc@hotmail.com.br>
2025-08-12 12:44:10 -0300

    Cont ferramenta enum4linux

--- src/database/functions/usuario.ts
+++ src/database/functions/usuario.ts
@@ -10,7 +10,7 @@ export const adicionarUsuarios = async (usuarios: TipoUsuario[], ipId: number) =
   });
   const novosUsuarios = usuarios.filter((u, i, arr) => arr.findIndex(e => e.nome === u.nome) === i)
   for (let i = 0; i < novosUsuarios.length; i++) {
-    const nome = usuarios[i].nome;
+    const nome = novosUsuarios[i].nome;
     const usuarioAtual = usuariosExistentes.find(u => u.nome === nome);
     if (!usuarioAtual && nome) {
       await prisma.usuario.create({
